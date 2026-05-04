f9ed1f27371a5307db2dc7b2be61ea6d38468baa
Shenuri Buddika <shenuribuddika@gmail.com>
2026-05-04 20:33:58 +0530

    Initial project structure

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    static ArrayList<String> students = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Student Record Manager");
+    }
+}
